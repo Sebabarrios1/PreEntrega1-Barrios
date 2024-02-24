f46782a47764d7340151842106003b6550948684
SebaBarrios <sebabarrios1985@gmail.com>
2024-02-24 04:41:49 -0300

    PreEntrega1-Barrios

---
+++ ecommarce/src/components/Carrito.jsx
@@ -0,0 +1,9 @@
+import carro from "../assets/carrito.png";
+const Carrito = () => {
+    return (<><div className="flex justify-center align-middle">
+        <img className="w-20" src={carro} />
+        <span className="text-xl">(19)</span>
+    </div></>);
+}
+
+export default Carrito;
---
+++ ecommarce/src/components/ListaItems.jsx
@@ -0,0 +1,6 @@
+const ListaCarrito = ({greeting}) => {
+    return (<><div className="text-xl flex justify-center"> <span>{greeting}</span>
+    </div></>);
+}
+
+export default ListaCarrito;
